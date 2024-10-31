80815e27838a30eef299dd8830f3cbf87d975f08
s3496h <s3496h@gmail.com>
2024-10-31 15:23:01 +0900

    member/me 추가, tests

--- src/test/java/com/koreait/rest_24_10/boundedContext/member/MemberControllerTests.java
+++ src/test/java/com/koreait/rest_24_10/boundedContext/member/MemberControllerTests.java
@@ -14,6 +14,7 @@ import org.springframework.test.web.servlet.ResultActions;
 
 import java.nio.charset.StandardCharsets;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -48,9 +49,24 @@ public class MemberControllerTests {
                 .andExpect(jsonPath("$.resultCode").value("S-1"))
                 .andExpect(jsonPath("$.msg").exists())
                 .andExpect(jsonPath("$.data.accessToken").exists());
-      /*  MvcResult mvcResult = resultActions.andReturn();
-        MockHttpServletResponse response = mvcResult.getResponse();
-        String authentication = response.getHeader("Authentication");
-        assertThat(authentication).isNotEmpty();*/
+
+    }
+
+    @Test
+    @DisplayName("GET /member/me 는 myPage")
+    void t2() throws Exception {
+        //When
+        ResultActions resultActions = mvc.perform(
+                        get("/api/v1/member/me")
+                )
+                .andDo(print());
+
+        //Then
+        resultActions.andExpect(status().is2xxSuccessful())
+                .andExpect(jsonPath("$.resultCode").value("S-1"))
+                .andExpect(jsonPath("$.msg").exists())
+                .andExpect(jsonPath("$.data.member.id").exists())
+                .andExpect(jsonPath("$.data.member.username").exists());
+
     }
 }
