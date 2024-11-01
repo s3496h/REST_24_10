252716f81c724fda6d1a02f1923ccfd09df0d14d
s3496h <s3496h@gmail.com>
2024-11-01 14:39:24 +0900

    spring doc 추가

---
+++ src/main/java/com/koreait/rest_24_10/base/BaseConfig.java
@@ -0,0 +1,13 @@
+package com.koreait.rest_24_10.base;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+@Configuration
+public class BaseConfig {
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
--- src/main/java/com/koreait/rest_24_10/base/security/ApiSecurityConfig.java
+++ src/main/java/com/koreait/rest_24_10/base/security/ApiSecurityConfig.java
@@ -1,19 +1,24 @@
 package com.koreait.rest_24_10.base.security;
 
+import com.koreait.rest_24_10.base.security.filter.JwtAuthorizationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
+
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class ApiSecurityConfig {
+    private final JwtAuthorizationFilter jwtAuthorizationFilter;
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
@@ -29,11 +34,12 @@ public class ApiSecurityConfig {
                 .formLogin().disable() // 폼 로그인 방식 끄기
                 .sessionManagement(sessionManagement ->
                         sessionManagement.sessionCreationPolicy(STATELESS)
-                ); // 세션 끄기
+                ) // 세션 끄기
+                .addFilterBefore(
+                        jwtAuthorizationFilter, // 액세스 토큰으로부터 로그인 처리
+                        UsernamePasswordAuthenticationFilter.class
+                );
+
         return http.build();
-    }
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
     }
 }
